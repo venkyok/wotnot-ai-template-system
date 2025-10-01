a62df7cb528d839392d4ff7ed06afc50fba9a567
venkatesh <vy8464@gmail.com>
2025-10-01 11:13:47 +0530

    Fix Render Rust compilation errors: Create compatible requirements without Rust dependencies

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Render.com build script
+# Render.com build script - use compatible requirements
 cd backend
-pip install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements-render.txt
